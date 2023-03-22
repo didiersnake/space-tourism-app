1587bcac504e1037a37eb6a298cc017da2a85370
didiersnake <didier.djakoua@gmail.com>
2023-03-22 18:31:13 +0100

    destination completed

--- src/components/DesttinationC.js
+++ src/components/DesttinationC.js
@@ -1,18 +1,64 @@
 import React, { useState } from "react";
-import "./destinationC.css"
+import "./destinationC.css";
 import { useSelector } from "react-redux";
 import { selectAllData } from "./destSlice";
 
-
 function DesttinationC() {
-  const [tab, setTab] = useState(1);
-    const data = useSelector(selectAllData)
-    const destData = data.destinations
+  const [tab, setTab] = useState(() => 1);
+  const data = useSelector(selectAllData);
+  const destData = data.destinations;
 
-    
   const toggleTabs = (index) => {
-    return setTab(index);
-  };
+      return setTab(index);
+    };
+      
+    const planet = destData.map((item, index) => {
+      return (
+        index + 1 === tab && (
+          <img
+            key={item.name}
+            src={require(`../assets/destination/${item.images["png"]}`)}
+            alt="imagePlanet"
+          />
+        )
+      );
+    });
+
+    const planetTabs = destData.map((item, index) => {
+      return (
+        <button
+          key={item.name}
+          className={index + 1 === tab ? "active-tab tab " : "tab"}
+          onClick={() => toggleTabs(index + 1)}
+        >
+          {item.name}
+        </button>
+      );
+    });
+
+    //planet content
+    const planetDetails = destData.map((item, index) => {
+      return (
+        index + 1 === tab && (
+          <div className="tab-content" key={item.name}>
+            <h1>{item.name}</h1>
+            <p>{item.description}</p>
+            <div className="details">
+              <div>
+                <p className="lil">Avg distance</p>
+                <p>{item.distance}</p>
+              </div>
+              <div>
+                <p className="lil">Est. Travel time</p>
+                <p>{item.travel}</p>
+              </div>
+            </div>
+          </div>
+        )
+      );
+    });
+
+
   return (
     <div className="destination-container">
       <div className="num">
@@ -20,51 +66,16 @@ function DesttinationC() {
       </div>
 
       <div className="content">
-        <div className="iamge-container">
-        {
-            destData.map((item, index) => {
-                return index + 1 === tab &&
-                    <img key={item.name} 
-                    src={require(`../assets/destination/${item.images["png"]}`)} alt="" />;
-            })
-        }
-        </div>
+        {planet}
 
-        <div tab-container>
+        <div className="tab-container">
           <div className="tabs-block">
-            {destData.map((item, index) => {
-              return <button
-              className={ index + 1 === tab ? "active-tab tab " : "tab"}
-              onClick={() => toggleTabs(index + 1)}
-              >{item.name}</button>
-          })}
-        </div>
+            {planetTabs}
+          </div>
 
-            <div>
-            {
-                destData.map((item, index)=> {
-                    return (
-                      index + 1 === tab && (
-                        <div className="tab-content" key={item.name}>
-                          <h1>{item.name}</h1>
-                          <p>{item.description}</p>
-                          <div className="details">
-                            <div>
-                              <p className="lil">Avg distance</p>
-                              <p>{item.distance}</p>
-                            </div>
-                            <div>
-                              <p className="lil">Est. Travel time</p>
-                              <p>{item.travel}</p>
-                            </div>
-                          </div>
-                        </div>
-                      )
-                    );
-                })
-            }
-            </div>
-          
+          <>
+            {planetDetails}
+          </>
         </div>
       </div>
     </div>
